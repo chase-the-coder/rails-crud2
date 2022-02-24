ae235606a5688d28597ee63603112cca5e3871e2
C <chase.lindsay1@gmail.com>
2022-02-23 18:58:19 -0600

    Added crud to model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,16 @@ class Restaurant < ApplicationRecord
     # validates :name, presense: true
     # validates :address, presense:true
     # validates :rating, inclusion: {in: 1..5}
+    def index
+    end
+    def new
+    end
+    def create
+    end
+    def edit
+    end
+    def update
+    end
+    def destroy
+    end
 end
